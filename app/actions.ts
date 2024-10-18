bf17b079b4bf6de2e2f5a0ad0b54b37ab93d9a49
Noah Korner <noah.korner@rsmus.com>
2024-10-18 14:40:27 -0500

    Fixes routes

--- app/actions.ts
+++ app/actions.ts
@@ -64,7 +64,7 @@ export const forgotPasswordAction = async (formData: FormData) => {
   }
 
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: `${origin}/auth/callback?redirect_to=/protected/reset-password`,
+    redirectTo: `${origin}/auth/callback?redirect_to=/${routes.authorized.resetPassword}`,
   });
 
   if (error) {
